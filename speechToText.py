8dea7be0d896a5f043b9c6e12eb5898abf163982
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-12-21 19:38:38 +0530

    Finally the listening function is working for the custom keypess duration!!!!

--- speechToText.py
+++ speechToText.py
@@ -2,36 +2,60 @@ import speech_recognition as sr
 import pyttsx3
 import keyboard
 
+import time
+
 # Initialize the recognizer
 recognizer = sr.Recognizer()
+recognizer.energy_threshold = 300
 
-# Function to listen to the microphone
-def record_text():
-    with sr.Microphone() as source:
-        print("Listening...")
-        print('Press Enter to stop Listening')
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
-        keyboard.wait('Enter')
-        
-    try:
-        print("Recognizing...")
-        text = recognizer.recognize_google(audio)
-        return text
-    except sr.UnknownValueError:
-        return "Could not understand audio"
-    except sr.RequestError as e:
-        return f"Error: {e}"
-    
-#function to speak what it listens
-def speak_text(command):
+# text to audio ==>
+def speak_text(text):
     engine = pyttsx3.init()
-    engine.say(command)
+    engine.say(text)
     engine.runAndWait()
 
+# Function to listen to the microphone with key binding
+def listen_untill_release():
+    print("Speak now. Press and hold 'Enter' to record, release to stop.")
+
+    audio_data = bytearray()  # Store raw audio data here
+
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        
+        while keyboard.is_pressed('enter'):
+            try:
+                audio = recognizer.record(source,duration=1)
+                audio_data.extend(audio.get_raw_data())  # Add raw audio data to the bytearray
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print(f"Error: {e}")
+        print("Got out of the listening Phase, Now converting")
+    # Create an AudioData object from the concatenated raw audio data
+    combined_audio = sr.AudioData(bytes(audio_data), source.SAMPLE_RATE, source.SAMPLE_WIDTH)
+
+    # Perform recognition on the combined audio
+    try:
+        text = recognizer.recognize_google(combined_audio)
+        print("Recognition complete.")
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+        text = None
+    except sr.RequestError as e:
+        print(f"Error: {e}")
+        text = ""
+
+    return text
+
+
 # Main loop
-while True:
-    input("Press Enter to start speaking ")
-    spoken_text = record_text()
-    print(spoken_text)
-    speak_text(spoken_text)
+if __name__ == '__main__':
+    while True:
+        if keyboard.is_pressed('enter'):
+            listened_text = listen_untill_release()
+            if(not listened_text):
+                print("Please speak again")
+                continue
+            print(listened_text)
+            speak_text(listened_text)
